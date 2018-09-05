5b5df04ff88b2dcc8016f5845b42235fc5281f4d
guo <guo@optimus-pro15-8.home>
2018-09-05 23:23:54 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,56 @@
+module github.com/bigg01/ocp-netpol-oberver
+
+require (
+	github.com/PuerkitoBio/purell v1.1.0 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/emicklei/go-restful v2.8.0+incompatible // indirect
+	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-openapi/jsonpointer v0.0.0-20180825180259-52eb3d4b47c6 // indirect
+	github.com/go-openapi/jsonreference v0.0.0-20180825180305-1c6a3fa339f2 // indirect
+	github.com/go-openapi/spec v0.0.0-20180825180323-f1468acb3b29 // indirect
+	github.com/go-openapi/swag v0.0.0-20180825180336-0dc164b7900e // indirect
+	github.com/gogo/protobuf v1.1.1 // indirect
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
+	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c // indirect
+	github.com/google/gofuzz v0.0.0-20170612174753-24818f796faf // indirect
+	github.com/googleapis/gnostic v0.2.0 // indirect
+	github.com/gregjones/httpcache v0.0.0-20180305231024-9cad4c3443a7 // indirect
+	github.com/howeyc/gopass v0.0.0-20170109162249-bf9dde6d0d2c // indirect
+	github.com/hpcloud/tail v1.0.0 // indirect
+	github.com/imdario/mergo v0.3.6 // indirect
+	github.com/json-iterator/go v1.1.5 // indirect
+	github.com/juju/ratelimit v1.0.1 // indirect
+	github.com/karrick/godirwalk v1.7.3 // indirect
+	github.com/mailru/easyjson v0.0.0-20180823135443-60711f1a8329 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/onsi/ginkgo v1.6.0 // indirect
+	github.com/onsi/gomega v1.4.1 // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/ramya-rao-a/go-outline v0.0.0-20170803230019-9e9d089bb61a // indirect
+	github.com/sirupsen/logrus v1.0.6
+	github.com/spf13/pflag v1.0.2 // indirect
+	github.com/stretchr/testify v1.2.2 // indirect
+	github.com/uudashr/gopkgs v1.3.2 // indirect
+	golang.org/x/crypto v0.0.0-20180904163835-0709b304e793 // indirect
+	golang.org/x/net v0.0.0-20180826012351-8a410e7b638d // indirect
+	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f // indirect
+	golang.org/x/sys v0.0.0-20180905080454-ebe1bf3edb33 // indirect
+	golang.org/x/text v0.3.0 // indirect
+	golang.org/x/tools v0.0.0-20180904205237-0aa4b8830f48 // indirect
+	gopkg.in/airbrake/gobrake.v2 v2.0.9 // indirect
+	gopkg.in/fsnotify.v1 v1.4.7 // indirect
+	gopkg.in/gemnasium/logrus-airbrake-hook.v2 v2.1.2 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+	gopkg.in/yaml.v2 v2.2.1 // indirect
+	k8s.io/api v0.0.0-20180816132701-f5c295feaba2 // kubernetes-1.9.10
+	k8s.io/apimachinery v0.0.0-20180627070101-fb40df2b5029 // kubernetes-1.9.10
+	k8s.io/client-go v0.0.0-20180806133048-cabe5c16b786 // kubernetes-1.9.10
+	k8s.io/kube-openapi v0.0.0-20180731170545-e3762e86a74c // indirect
+
+)
---
+++ main.go
@@ -0,0 +1,116 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Note: the example only works with the code within the same release/branch.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	// Uncomment the following line to load the gcp plugin (only required to authenticate against GKE clusters).
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+)
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	//log.SetFormatter(&log.JSONFormatter{})
+
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.InfoLevel)
+}
+
+func main() {
+	var kubeconfig *string
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	for {
+		//pods, err := clientset.CoreV1().NetworkPolicy("").List(metav1.ListOptions{})
+		netpol, err := clientset.NetworkingV1().NetworkPolicies("").List(metav1.ListOptions{})
+		//clientset.CoreV1().
+		//list := &networkingv1.NetworkPolicyList{ListMeta: obj.(*networkingv1.NetworkPolicyList).ListMeta}
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("There are %d netpol in the cluster\n", len(netpol.Items))
+
+		for _, np := range netpol.Items {
+			//log.Info(np.Name)
+
+			/*
+				{"metadata":{"name":"web-allow-all-namespaces","namespace":"np","selfLink":"/apis/networking.k8s.io/v1/namespaces/np/networkpolicies/web-allow-all-namespaces","uid":"718810d5-b14c-11e8-91fd-023c04a44d83","resourceVersion":"3675","generation":1,"creationTimestamp":"2018-09-05T20:44:11Z"},
+				"spec":{"podSelector":{"matchLabels":{"app":"web"}},
+				"ingress":[{"from":[{"namespaceSelector":{}}]}],"policyTypes":["Ingress"]}}
+			*/
+
+			log.WithFields(log.Fields{
+				"Ingress":     np.Spec.Ingress,
+				"PodSelector": np.Spec.PodSelector,
+			}).Info(np.Name, np.Namespace)
+
+			/*
+				data, err := json.Marshal(np.Spec.Ingress)
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf("%s\n", data)
+			*/
+
+			// Examples for error handling:
+			// - Use helper functions like e.g. errors.IsNotFound()
+			// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+
+		}
+
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
+}
